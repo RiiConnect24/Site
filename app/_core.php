360df7ad6e45a43d56e7535c52ccfe6054a7c9fb
Larsenv <larsen@weborican.com>
2017-06-02 02:18:00 +0200

    Fixed stats

--- app/_core.php
+++ app/_core.php
@@ -1,7 +1,8 @@
 <?php
-require '../../../vendor/autoload.php'; // Composer
+require '/var/www/riiconnect24.net/public_html/vendor/autoload.php'; // Composer
 
 function rc24_initialise_twig($dir = '/var/www/riiconnect24.net/public_html/.jsatest/views/') {
-	$loader = new Twig_Loader_Filesystem($dir); // initialise Twig stuff
+	$loader = new Twig_Loader_Filesystem($dir, array('debug' => true)); // initialise Twig stuff
 	return new Twig_Environment($loader);
 }
+?>
--- app/vote/index.php
+++ app/vote/index.php
@@ -1,5 +1,5 @@
 <?php
-require '../../../vendor/autoload.php'; // Composer
+require '/var/www/riiconnect24.net/public_html/vendor/autoload.php'; // Composer
 require '../_core.php';
 $twig = rc24_initialise_twig();
 echo $twig->render('vote/index.html');
