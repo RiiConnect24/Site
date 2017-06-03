2161e45d218da26649241a5107be27694a2eb667
Joshua Kelly <jsa@t7w33zb.server.linevast.org>
2017-06-03 16:17:18 +0200

    yay

---
+++ app/goodies/wallpapers/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "xhtml11.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+<title>RiiConnect24 Wallpapers</title>
+
+<?php
+echo file_get_contents("/var/www/riiconnect24.net/public_html/head.html");
+?>
+<div class="wrapper style1 first">
+	<article class="container" id="top">
+		<div class="row">
+			<img width="500" src="/images/logo.png" alt="RiiConnect24 Logo" />
+			<header>
+			<h1><u><b>RiiConnect24 Themes</b></u></h1>
+			</header>
+			<p>We have made some wallpapers for you to use. You can download them here. <a href="/themes/">We also made some themes.</a></p>
+			<a href="#wallpapers" class="button big scrolly">View Wallpapers</a>
+		</div>
+	</article>
+</div>
+<div class="wrapper style2">
+  <article id="wallpapers">
+	<script type="text/javascript">
+    	hs.graphicsDir = '/highslide/graphics/';
+    	hs.outlineType = 'rounded-white';
+	</script>
+	<h1>RiiConnect24:</h1>
+	<center>
+		<div>
+		<a id="thumb1" href="riiconnect24_kuniharu_1_1920x1080.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="riiconnect24_kuniharu_1_1920x1080.png" width="25%" alt="Kuniharu Fujikawa Quote 1"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+		    Fujikawa Quote 1
+		</div>
+
+		<a id="thumb2" href="riiconnect24_kuniharu_2_1920x1080.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="riiconnect24_kuniharu_2_1920x1080.png" width="25%" alt="Kuniharu Fujikawa Quote 2"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+		    Fujikawa Quote 2
+		</div>
+
+		<a id="thumb3" href="riiconnect24_kouichi_1_1920x1080.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="riiconnect24_kouichi_1_1920x1080.png" width="25%" alt="Kouichi Kawamoto Quote 1"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+ 		   Kawamoto Quote 1
+		</div>
+
+		<a id="thumb4" href="riiconnect24_kouichi_2_1920x1080.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="riiconnect24_kouichi_2_1920x1080.png" width="25%" alt="Kouichi Kawamoto Quote 2"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+ 		   Kawamoto Quote 2
+		</div>
+
+		<a id="thumb5" href="riiconnect24_1920x1080.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="riiconnect24_1920x1080.png" width="25%" alt="RiiConnect24"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+			RiiConnect24
+		</div>
+	</center>
+	<h1>Check Mii Out Channel:</h1>
+	<center>
+		<div>
+		<a id="thumb6" href="CMO_Channel_BG_1600x900.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="CMO_Channel_BG_1600x900.png" width="25%" alt="Check Mii Out Channel Background"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+		    Check Mii Out Channel Background
+		</div>
+
+		<a id="thumb7" href="CMO_Channel_BG_orange_1600x900.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="CMO_Channel_BG_orange_1600x900.png" width="25%" alt="Check Mii Out Channel Orange Background"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+ 		   Check Mii Out Channel Orange Background
+		</div>
+
+		<a id="thumb8" href="CMO_Channel_BG_blue_1600x900.png" class="highslide" onclick="return hs.expand(this)">
+			<img src="CMO_Channel_BG_blue_1600x900.png" width="25%" alt="Check Mii Out Channel Blue Background"
+				title="Click to enlarge" /></a>
+
+		<div class="highslide-caption">
+		    Check Mii Out Channel Blue Background
+		</div>
+	</center>
+</div>
+<?php
+echo file_get_contents("/var/www/riiconnect24.net/public_html/footer.html");
+?>
+</div>
+</div>
+</body>
+</html>
