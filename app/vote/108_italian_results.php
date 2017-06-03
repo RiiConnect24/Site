4f8a9aba836ecdf063f4c343b13a2b4b8c95c1e7
Larsenv <larsen@weborican.com>
2017-06-03 18:13:06 +0200

    Votes added

---
+++ app/vote/108_italian_results.php
@@ -0,0 +1,8 @@
+<?php
+require '../../../../vendor/autoload.php'; // Composer
+require '../_core.php';
+$twig = rc24_initialise_twig();
+echo $twig->render('vote/108_italian_results.html');
+
+exit; // not really necessary, but nice to have :^)
+?>
