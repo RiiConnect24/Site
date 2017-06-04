b705abea6c473cfe29d1e8bbd2085c96341702cf
Larsenv <larsen@weborican.com>
2017-06-05 00:24:24 +0200

    Added votes

--- app/goodies/vote/030_spanish_results.php
+++ app/goodies/vote/030_spanish_results.php
@@ -1,8 +1,8 @@
 <?php
-require '../../../../vendor/autoload.php'; // Composer
-require '../_core.php';
+require '../../../../../vendor/autoload.php'; // Composer
+require '../../_core.php';
 $twig = rc24_initialise_twig();
-echo $twig->render('vote/030_spanish_results.html');
+echo $twig->render('goodies/vote/030_spanish_results.html');
 
 exit; // not really necessary, but nice to have :^)
 ?>
--- app/goodies/vote/098_english_results.php
+++ app/goodies/vote/098_english_results.php
@@ -1,8 +1,8 @@
 <?php
-require '../../../../vendor/autoload.php'; // Composer
-require '../_core.php';
+require '../../../../../vendor/autoload.php'; // Composer
+require '../../_core.php';
 $twig = rc24_initialise_twig();
-echo $twig->render('vote/098_english_results.html');
+echo $twig->render('goodies/vote/098_english_results.html');
 
 exit; // not really necessary, but nice to have :^)
 ?>
