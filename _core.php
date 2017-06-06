53218fc031af66df516798c82310e998ba1934b5
Larsenv <larsen@weborican.com>
2017-06-06 20:21:53 +0200

    Fixed stuff

--- _core.php
+++ _core.php
@@ -1,5 +1,5 @@
 <?php
-require '/var/www/riiconnect24.net/public_html/vendor/autoload.php'; // Composer
+require '../vendor/autoload.php'; // Composer
 
 function rc24_initialise_twig($dir = '/var/www/riiconnect24.net/public_html/views/') {
 	$loader = new Twig_Loader_Filesystem($dir, array('debug' => true)); // initialise Twig stuff
