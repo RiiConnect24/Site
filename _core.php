6057bd8c94f278d267f6b03d6cc68ef34dcbdec1
Larsenv <larsen@weborican.com>
2017-06-06 21:11:49 +0200

    Fixed vendor

--- _core.php
+++ _core.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/autoload.php'; // Composer
+require '/var/www/riiconnect24.net/vendor/autoload.php'; // Composer
 
 function rc24_initialise_twig($dir = '/var/www/riiconnect24.net/public_html/views/') {
 	$loader = new Twig_Loader_Filesystem($dir, array('debug' => true)); // initialise Twig stuff
