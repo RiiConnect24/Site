3d5b21ecdac591c026e189770981cbda91ea6527
larsenv <larsen@weborican.com>
2017-06-06 13:33:06 -0500

    Fixed votes

--- goodies/vote/index.php
+++ goodies/vote/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../../vendor/autoload.php'; // Composer
-require '../../_core.php';rc24_initialise_twig();
+require '../../_core.php';
+$twig = rc24_initialise_twig();
 echo $twig->render('goodies/vote/index.html');
 
 exit; // not really necessary, but nice to have :^)
