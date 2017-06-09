e18f30e45a815637a76784883360468a3b96f812
Spotlight <awesomebing1@gmail.com>
2017-06-08 19:21:48 -0500

    Merge the files that I kind of forgot to merge

--- js/interview_cmn.js
+++
@@ -1,6 +0,0 @@
-JSpath = CSSpath_loc.replace("css/", "");
-
-document.write('<SCRIPT LANGUAGE="JavaScript" TYPE="text/JavaScript" SRC="'+ JSpath +'js/scroll.js"></SCRIPT>');
-document.write('<SCRIPT LANGUAGE="JavaScript" TYPE="text/JavaScript" SRC="'+ JSpath +'js/select_style.js"></SCRIPT>');
-document.write('<SCRIPT LANGUAGE="JavaScript" TYPE="text/JavaScript" SRC="'+ JSpath +'js/jquery.js"></SCRIPT>');
-document.write('<SCRIPT LANGUAGE="JavaScript" TYPE="text/JavaScript" SRC="'+ JSpath +'js/jquery_auto.js"></SCRIPT>');
--- js/interview_noscript.js
+++
@@ -1,6 +0,0 @@
-/* noscript bug fix */
-
-document.write (
-	'<style type="text/css">' +
-	'span.noscript_cont { display:none; }' +
-	'</style>');
