8ac1fe14b9f81687f4a69c5103d3d4a618e029ad
larsenv <larsen@weborican.com>
2017-07-05 14:50:21 -0500

    Fixed core

--- services/forecast.php
+++ services/forecast.php
@@ -2,7 +2,7 @@
 require getcwd() . 'vendor/autoload.php'; // Composer
 require '../_core.php';
 $twig = rc24_initialise_twig();
-echo $twig->render('service_forecast.html');
+echo $twig->render('services/forecast.html');
 
 exit; // not really necessary, but nice to have :^)
 ?>
