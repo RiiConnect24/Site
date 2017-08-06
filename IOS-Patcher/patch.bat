0a0c301af0f2346ca0d039478c3dfe0e0a8c346a
KcrPL <thekoziels@gmail.com>
2017-08-06 22:39:09 +0200

    Preperation for Auto Update feature in IOS Patcher

---
+++ IOS-Patcher/patch.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+goto 1
+:1
+cls
+echo It seems that update is successfull.
+pause>NUL
+goto 1
--- IOS-Patcher/test.bat
+++
@@ -1,4 +0,0 @@
-This is only test file.
-It has nothing to do with IOS Patcher.
-
-I need it now only to test the downloading update feature.
