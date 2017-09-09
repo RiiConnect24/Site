6d6405ec7e395486211f3008a1c5e860b7597628
larsenv <larsen@weborican.com>
2017-09-09 14:00:55 -0500

    Fixed links

--- instructions/wiiu.php
+++ instructions/wiiu.php
@@ -9,8 +9,8 @@ header("Location: https://wii.guide/riiconnect24");
     <div class="section no-pad-bot">
       <div class="container center">
         <h2>We have moved our instructions to wii.guide.</h2>
-        <meta http-equiv="refresh" content="0; url=https://wii.guide/docs/Wii/RiiConnect24" />
-        <a href="https://wii.guide/docs/Wii/RiiConnect24">Click here if you're not redirected.</a>
+        <meta http-equiv="refresh" content="0; url=https://wii.guide/riiconnect24" />
+        <a href="https://wii.guide/riiconnect24">Click here if you're not redirected.</a>
       </div>
     </div>
   </body>
